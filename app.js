583c8e168a356804439697bfd9bdcf458ec98107
Kauan Marchezi <kauanmarchezi@gmail.com>
2025-02-12 16:12:34 -0300

    Deixando o array visual

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 let listaDeAmigos = [];
 
-
+//Adiciona um novo nome no array
 function adicionarAmigo() {
     let novoNome = document.getElementById('amigo').value;
     if (novoNome < 1) {
         alert('Por favor, digite um nome válido!');
     }
-    listaDeAmigos.push(novoNome);
-    document.getElementById('amigo').value = [];
+    if (listaDeAmigos.includes(novoNome)) {
+        alert('Este nome já está na lista, digite outro nome');
+        } else {
+            listaDeAmigos.push(novoNome);
+            atualizarLista();
+        }
+        document.getElementById('amigo').value = [];
+    }
+
+//Adiciona o nome inserido no input como um elemento <li/> dentro de uma lista no HTML exibida ao usuário
+function atualizarLista() {
+    let lista = document.getElementById('listaAmigos'); 
+    lista.innerHTML = ''; 
+    for (i = 0; i < listaDeAmigos.length; i++) {
+        let novoItem = document.createElement('li');
+        novoItem.textContent = listaDeAmigos[i];
+        lista.appendChild(novoItem);
+    }
+
 }
+
 
