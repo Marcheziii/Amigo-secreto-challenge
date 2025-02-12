9b0a5862594fe4604bf33ae1c224512ffa0c768f
Kauan Marchezi <kauanmarchezi@gmail.com>
2025-02-12 18:35:18 -0300

    Função sortear amigo + alterações

--- app.js
+++ app.js
@@ -3,17 +3,21 @@ let listaDeAmigos = [];
 //Adiciona um novo nome no array
 function adicionarAmigo() {
     let novoNome = document.getElementById('amigo').value;
-    if (novoNome < 1) {
+    //Verifica se o usuário não digitou apenas um caractere
+    if (novoNome.length <= 1) {
         alert('Por favor, digite um nome válido!');
+        document.getElementById('amigo').value = [];
+        return;
     }
+    //Verifica se já não existe um nome igual no array
     if (listaDeAmigos.includes(novoNome)) {
         alert('Este nome já está na lista, digite outro nome');
-        } else {
-            listaDeAmigos.push(novoNome);
-            atualizarLista();
-        }
-        document.getElementById('amigo').value = [];
+    } else {
+        listaDeAmigos.push(novoNome);
+        atualizarLista();
     }
+    document.getElementById('amigo').value = [];
+}
 
 //Adiciona o nome inserido no input como um elemento <li/> dentro de uma lista no HTML exibida ao usuário
 function atualizarLista() {
@@ -24,7 +28,18 @@ function atualizarLista() {
         novoItem.textContent = listaDeAmigos[i];
         lista.appendChild(novoItem);
     }
+}
 
+//Sortear um amigo aleatório
+function sortearAmigo() {
+    let mensagemResultado = document.getElementById('resultado');
+    mensagemResultado.innerHTML = '';
+    let numeroSorteado = Math.floor((Math.random () * listaDeAmigos.length));
+    if (listaDeAmigos.length < 1) {
+        alert('Não há amigos para sortear.');
+    } else {
+        mensagemResultado.innerHTML ='O amigo(a) secreto sorteado(a) é: ' + listaDeAmigos[numeroSorteado];
+    }
 }
 
 
